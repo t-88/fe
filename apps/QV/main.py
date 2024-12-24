fe828a4569849db1c9f4981ff81b36f7fbc77e4f
t-88 <temptaki5002@gmail.com>
2024-12-24 16:38:18 +0100

    Add quick vim utility

--- apps/QV/main.py
+++ apps/QV/main.py
@@ -1,18 +1,21 @@
 import os
 from time import gmtime, strftime
+import datetime
 
 DB_PATH =f"{os.path.expanduser('~')}/.config/fe"
 SAVE_PATH = "vim-tmps"
+DIR_PATH = f"{DB_PATH}/{SAVE_PATH}"
 
-sufix_file_name = strftime("%Y-%m-%d-%H:%M:%S", gmtime())
-file_name = f"{DB_PATH}/{sufix_file_name}" 
-
-
-if not os.path.exists(file_name):
-    os.mkdir(file_name)
-
-if not os.path.exists(file_name):
-    os.mkdir(file_name)
+sufix_file_name =  datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S') 
+file_name = f"{DIR_PATH}/{sufix_file_name}" 
 
 
-s.open
+if not os.path.exists(DIR_PATH):
+    os.mkdir(DIR_PATH)
+
+with open(file_name,"w"):
+    pass
+
+
+os.system("vim " + file_name)
+
